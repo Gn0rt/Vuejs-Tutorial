525c9a5debc59fd72d51566e7bf274f3059cd02e
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-07-18 00:39:28 +0700

    router

--- Tutorial/src/views/Contact.vue
+++ Tutorial/src/views/Contact.vue
@@ -1,6 +1,14 @@
 <script setup>
+import { useRouter } from 'vue-router';
+
+
+    const router = useRouter();
+
 </script>
 
 <template>
     <h1>Contact Page</h1>
+    <button @click="router.push({name: 'us'})">Redirect</button>
+
+    <!-- <RouterView /> -->
 </template>
