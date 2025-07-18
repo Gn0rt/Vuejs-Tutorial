b2a5c1ddfc5d75da7489e598fb5085b21f1ac80a
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-07-18 16:42:23 +0700

    props

--- Tutorial/src/components/TheWelcome.vue
+++ Tutorial/src/components/TheWelcome.vue
@@ -1,7 +1,8 @@
 <script setup>
-
+const props = defineProps(['count']);
 </script>
 
 <template>
   <h1>The Welcome</h1>
+  <p>This count is: {{ props.count }}</p>
 </template>
--- Tutorial/src/views/Contact.vue
+++ Tutorial/src/views/Contact.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import TheWelcome from '@/components/TheWelcome.vue';
+const router = useRouter();
 
-
-    const router = useRouter();
+const count = ref(0);
 
 </script>
 
 <template>
     <h1>Contact Page</h1>
     <button @click="router.push({name: 'us'})">Redirect</button>
-
+    <the-welcome :count="count"></the-welcome>
     <!-- <RouterView /> -->
 </template>
