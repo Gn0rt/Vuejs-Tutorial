750193b2c1f128151526545567a3d4a33779571c
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-07-21 23:33:06 +0700

    create new project vue

--- Tutorial/src/components/WelcomeItem.vue
+++ Tutorial/src/components/WelcomeItem.vue
@@ -1,5 +1,17 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+const count = ref(0);
+function increment() {
+  count.value++;
+}
+
+onMounted(()=> {
+  console.log("So lần nhấp ban đầu là ", count.value);
+})
+</script>
 <template>
   <h1>Welcome Item</h1>
+  <button @click="increment">Số lần nhấp {{ count }}</button>
 </template>
 
 <style scoped>
