d657422f15ec910fbd2b04538298458deff3431e
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-07-23 17:21:10 +0700

    v-for, v-if

--- 02-vue/src/App.vue
+++ 02-vue/src/App.vue
@@ -1,85 +1,105 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive } from 'vue';
+const isLoggedIn = ref(false);
+const log = () => {
+  isLoggedIn.value = !isLoggedIn.value;
+};
+
+const userStatus = ref('online');
+const changeStatus = () => {
+  if(userStatus.value === 'online') {
+    userStatus.value = 'away';
+  }else if (userStatus.value === 'away') {
+    userStatus.value = 'busy';
+  } else if (userStatus.value === 'busy') {
+    userStatus.value = 'online';
+  }
+}
+
+const isVisible = ref(true);
+const toggleVisibility = () => {
+  isVisible.value = !isVisible.value;
+}
+
+const shoppingItems = ref([
+  {name: 'Apple', price: 7},
+  {name: 'Banana', price: 5},
+  {name: 'Orange', price: 12},
+])
+const user = ref({
+  firstName: 'Trong',
+  lastName: 'Bui'
+});
+const users = ref([
+{
+  firstName: 'Trong',
+  lastName: 'Bui',
+  age: 22,
+  isActive: true
+},
+{
+  firstName: 'Nam',
+  lastName: 'Bui',
+  age: 30,
+  isActive: true
+},
+{
+  firstName: 'Khuong',
+  lastName: 'Bui',
+  age: 55,
+  isActive: false
+}
+]);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h1>02 - vue</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <h3 v-if="isLoggedIn">Chào mừng người dùng đã đăng nhập!</h3>
+  <h3 v-else>Chào mừng khách!</h3>
+  <button @click="log">{{ isLoggedIn ? "Đăng xuất" : "Đăng nhập" }}</button>
+  <div :style="{marginTop: '20px'}">
+    <h2>V-if - Thay đổi hẳn phần tử trong DOM</h2>
+    <p v-if="userStatus === 'online'">Người dùng đang online</p>
+    <p v-else-if="userStatus === 'away'">Người dùng đang vắng mặt</p>
+    <p v-else-if="userStatus === 'busy'">Người dùng đang bận</p>
+    <p v-else>Không rõ trạng thái</p>
+    <button @click="changeStatus">Thay đổi trạng thái</button>
+  </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div :style="{marginTop: '20px'}">
+    <h2>V-show - Chỉ ẩn hiện các phần tử như "display:none"</h2>
+    <p v-show="isVisible">Nội dung này ẩn hoặc hiển thị</p>
+    <button @click="toggleVisibility">{{ isVisible ? "Ẩn" : "Hiện" }}</button>
+  </div>
 
-  <RouterView />
+
+  <div :style="{marginTop: '20px'}">
+    <h2>V-for</h2>
+    <ul>
+      <li v-for="item in shoppingItems" :key="item.name">
+        {{ item.name }} - {{ item.price }}$
+      </li>
+    </ul>
+
+    <span v-for="n in 10" :key="n">{{ n }}</span>
+
+    <ul>
+      <li v-for="(value, key, index) in user" :key="key">
+        {{ key }} : {{ value }} (Index: {{ index }})
+      </li>
+    </ul>
+
+    <ul>
+      <template v-for="(user, index) in users">
+        <li v-if="user.isActive" :key="user.firstName">
+          {{ user.firstName + " " + user.lastName }} - {{ user.isActive ? "Active" : "Inactive" }}
+        </li>
+      </template>
+      
+    </ul>
+  </div>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ 03-todo/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+const todos = ref([
+  { id: 1, text: 'Learn Vue.js', completed: false },
+  { id: 2, text: 'Build a Todo App', completed: false },
+  { id: 3, text: 'Deploy the App', completed: false }
+]);
+</script>
+
+<template>
+  <h1>Todo</h1>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }} -
+        <span v-if="todo.completed">Completed</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
