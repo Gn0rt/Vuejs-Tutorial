c4af7f2412db046f71462b6e243397f535c038ff
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-07-23 15:18:15 +0700

    cmt
    done basic syntax vue

--- 01-first-project/src/App.vue
+++ 01-first-project/src/App.vue
@@ -55,6 +55,49 @@ const change2 = () => {
   obj2.count++;
   obj2.user.age++;
 }
+
+const firstName = ref("Trong");
+const lastName = ref("Bui");
+
+const fullName = computed({
+  get() {
+    return `${firstName.value} ${lastName.value}`;
+  },
+  set(newValue) {
+    firstName.value = newValue.split(",")[0];
+    lastName.value = newValue.split(",")[1];
+  }
+});
+
+function changeFullName() {
+  fullName.value = "Bui The, Nam";
+}
+
+const isActive = ref(true);
+const hasError = ref(false);
+
+const classObject = reactive({
+  active: false,
+  'has-error': true
+})
+
+const classObject2 = computed(() => ({
+  active: isActive.value,
+    'has-error': hasError.value
+}))
+
+const changeActive = () => {
+  isActive.value = !isActive.value;
+  hasError.value = !hasError.value;
+}
+
+const color = ref('red');
+const fontSize = ref('24px');
+
+const styleObject = reactive({
+  color: 'orange',
+  fontSize: '26px'
+})
 </script>
 
 <template>
@@ -93,8 +136,75 @@ const change2 = () => {
     <p>Array: {{  obj2.array }}</p>
     <button @click="change2">Increase</button>
   </div>
+
+  <div>
+    <p>Fist Name: {{ firstName }}</p>
+    <p>Last Name: {{ lastName }}</p>
+    <p>Full name: {{ fullName }}</p>
+    <button @click="changeFullName">Change Full Name</button>
+  </div>
+
+
+  <div>
+    <button @click="changeActive" class="button" :class="classObject2">{{ isActive ? "Active" : "Error" }}</button>
+    <button @click="changeActive" class="button" :class="[isActive ? 'active' : '', {'has-error': hasError}]">{{ isActive ? "Active" : "Error" }}</button>
+  
+  </div>
+
+  <div>
+    <h1 :style="{
+        color: 'blue',
+        fontSize: '20px',
+        }">
+      Binding Style
+    </h1>
+    <h1 :style="{
+        color,
+        fontSize
+      }">
+      Binding Style Ref
+    </h1>
+    <h1 :style="styleObject">Binding Style Reactive</h1>
+    <h1 :style="[
+      {
+        backgroundColor: 'blue',
+        fontSize: '30px',
+        
+      },
+      styleObject
+    ]">Binding Style Merge</h1>
+  </div>
 </template>
 
 <style scoped>
+.button {
+  margin-top: 30px;
+  border: none;
+  background-color: #ede9fe;
+  color: blueviolet;
+  font-size: 16px;
+  border-radius: 6px;
+  padding: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #7a2b8c;
+    color: #fff;
+  }
+}
+.active {
+  background-color: blueviolet;
+  color: #fff;
+  cursor: pointer;
+  &:hover {
+    background-color: #7a2b8c;
+  }
+}
 
+.has-error {
+  background: red;
+  color: #fff;
+  &:hover {
+    background: rgb(243, 82, 82);
+  }
+}
 </style>
