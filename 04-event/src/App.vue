fe59985b92b62058b39b46487046c094172f6c6f
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-07-26 00:17:58 +0700

    stop, prevent, self
    stop: ngăn chặn nổi bọt
    prevent: ngăn chặn các thuộc tính mặc định của website
    self: chỉ thực hiện hàm đó

--- 04-event/src/App.vue
+++ 04-event/src/App.vue
@@ -1,9 +1,53 @@
 <script setup>
 
+import { ref, reactive } from 'vue';
+const count = ref(0);
+
+
+const greet = (e) => {
+  console.log(e.target);
+  alert('Hello World');
+}
+
+const sayHi = (name) => {
+  alert(`Hi ${name}`);
+}
+const sayHi2 = (name) => {
+  alert(`Hi ${name}`);
+}
+
+const submit = () => {
+  alert('Submitted');
+}
 </script>
 
 <template>
-  <h1>Xử lý sự kiện - event</h1>
+  <h1>Xử lý sự kiện - event handling</h1>
+  <p>Count: {{ count }}</p>
+  <!-- <button v-on:click="count++">Tăng</button> -->
+  <button @click="greet">Chào</button>
+  <button @click="sayHi('Trong')">Say hi</button>
+
+  <div>
+    <h2>Event có đối số</h2>
+    <button @click="sayHi('B')">
+      <button @click.stop="sayHi2('C')">Click 2</button>
+      Click 1
+    </button>
+
+    <form @submit.prevent="submit" action="">
+      <button type="submit">Submit</button>
+    </form>
+
+    <div :style="{marginTop: '10px'}">
+      <button @click.self="sayHi('B')">
+        <button @click.self="sayHi2('C')">Click 2</button>
+        Click 1
+      </button>
+    </div>
+  </div>
+
+
 </template>
 
 <style scoped>
