9c4e72e596593b062c22fb8d8dc332eb1d57465d
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-07-26 14:48:11 +0700

    v-model and set up 06-dom

--- 05-form/src/App.vue
+++ 05-form/src/App.vue
@@ -1,85 +1,121 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive } from 'vue';
+
+const text = ref('none');
+const handleInput = (e) => {
+  console.log(e)
+  text.value = e.target.value;
+};
+
+const multiline = ref('');
+
+const checked = ref([]);
+const picked = ref('None');
+const selected = ref('None');
+const pickedOptions = reactive([
+  { id: 'one', value: 'One' },
+  { id: 'two', value: 'Two' },
+  { id: 'three', value: 'Three' }
+]);
+
+const selectedOptions = reactive([
+  { id: 'A', value: 'A' },
+  { id: 'B', value: 'B' },
+  { id: 'C', value: 'C' }
+]);
+
+const multipleCheck = ref('')
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h1>05 - Form Input Bindings</h1>
+  <p>Text: {{ text }}</p>
+  <input type="text" :value="text" @input="handleInput">
+  <!-- use v-model  -->
+  <input type="text" v-model="text">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <p>MultiLine Text: {{ multiline }}</p>
+  <textarea v-model="multiline" placeholder="Your message here"></textarea>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div>
+    <h1>Checkbox, radio, select</h1>
+    <div>
+      <p>Checkbox : {{ checked }}</p>
+      <input type="checkbox" id="jack" value="Jack97" v-model="checked">
+      <label for="jack">Jack</label>
+
+      <input type="checkbox" id="john" value="John" v-model="checked">
+      <label for="john">John</label>
+
+      <input type="checkbox" id="mike" value="Mike" v-model="checked">
+      <label for="mike">Mike</label>
     </div>
-  </header>
 
-  <RouterView />
+    <div>
+      <p>Picked (radio) : {{ picked }}</p>
+      <input type="radio" id="one" :value="'first'" v-model="picked">
+      <label for="one">MTP</label>
+
+      <input type="radio" id="two" :value="'second'" v-model="picked">
+      <label for="two">Jack</label>
+
+      <!-- use for -->
+       <h4 :style="{margin: '10px 0'}">Use v-for</h4>
+       <div v-for="option in pickedOptions" :key="option.id">
+        <input type="radio" :id="option.id" :value="option.value" v-model="picked">
+        <label :for="option.id">{{ option.value }}</label>
+       </div>
+    </div>
+
+    <div>
+      <p>Select: {{ selected }}</p>
+      <select v-model="selected">
+        <option disabled :value="{selected}">Please select one</option>
+        <option v-for="op in selectedOptions" :key="op.id">
+          {{ op.value }}
+        </option>
+      </select>
+    </div>
+
+  </div>
+
+  <div>
+    <h1>Multiple</h1>
+    <div>
+      <p>Checkbox: {{ multipleCheck }}</p>
+      <input 
+        type="checkbox"   
+        v-model="multipleCheck"
+        true-value="Yes"
+        false-value="No"
+      >
+        
+    </div>
+    <div>
+      <div>Selected: {{ selected }}</div>
+        <select v-model="selected">
+          <option :value="{'name': 'A'}">A</option>
+          <option :value="{'name': 'B'}">B</option>
+          <option :value="{'name': 'C'}">C</option>
+        </select>
+    </div>
+  </div>
+
+  <div>
+    <h1>Modifiers v-model</h1>
+    <p>Text: {{ text }}</p>
+    <p>Type of text: {{ typeof text }}</p>
+    <input type="text" v-model.lazy="text">
+
+    <!-- tự động chuyển sang dạng number -->
+    <input type="text" v-model.number="text">
+
+
+
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
