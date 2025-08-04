8b043546634aa703ec9d1d27733a0abb683326e5
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-08-05 00:02:21 +0700

    onBeforeMount and onBeforeUnmounted
    onBeforeMont: thực hiện trước khi được mount -> onMounted: được mount -> onBeforeUnmounted: trước khi unmounted, DOM vẫn còn, vẫn thực hiện được -> onUnmounted: unMount, thoát khỏi lifecycle

--- 07-lifecycle/src/App.vue
+++ 07-lifecycle/src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/before">Before Mounted</RouterLink>
+
       </nav>
     </div>
   </header>
---
+++ 07-lifecycle/src/views/BeforeMount.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+import { ref,onBeforeMount, onBeforeUnmount, onMounted, onUnmounted } from 'vue';
+
+const time = ref(0);
+let intervalId = null;
+
+onBeforeMount(() => {
+    console.log("OnbeforeMount");
+})
+
+onMounted(()=> {
+    console.log("OnMounted");
+    intervalId = setInterval(() => {
+        time.value++;
+    }, 1000);
+})
+
+onBeforeUnmount(()=> {
+    console.log("OnBeforeUnmount");
+    clearInterval(intervalId);
+})
+
+onUnmounted(()=> {
+    console.log("OnUnmounted");
+  
+})
+
+</script>
+
+<template>
+  <div class="about">
+    <h1>Time: {{ time }}</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
