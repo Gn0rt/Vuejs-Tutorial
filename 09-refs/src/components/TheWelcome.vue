e7be3ba6f74246d63b4ac526436f0ff84db31277
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-08-12 11:16:56 +0700

    template ref: tham chiếu trực tiếp đến một phần tử DOM cụ thể hoặc một thể hiện của thành phần con sau khi được mount

---
+++ 09-refs/src/components/TheWelcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+const inputRef = ref(null);
+
+onMounted(() => {
+  inputRef.value.focus();
+});
+console.log(inputRef);
+</script>
+
+<template>
+<h1>Template Refs</h1>
+<input ref="inputRef" type="text">
+</template>
